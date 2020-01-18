f238c3911ff782a6af57f87f9c42de334146df1a
Vinit Jogani <vnjogani@gmail.com>
2020-01-18 14:34:15 -0500

    Added memory view

--- memoryjane/lib/ui/detail.component.dart
+++ memoryjane/lib/ui/detail.component.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:memoryjane/entities/collection.dart';
+import 'package:memoryjane/ui/memory.component.dart';
 
 class DetailComponent extends StatelessWidget {
 
@@ -10,9 +11,10 @@ class DetailComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       body: Column(
         children: <Widget>[
-          SizedBox(height: 5,),
+          SizedBox(height: 15,),
           Row(
             children: <Widget>[
               IconButton(
@@ -35,12 +37,27 @@ class DetailComponent extends StatelessWidget {
               ),
             ),
           ),
-          ListView(
-            children: <Widget>[
-              
-            ],
+          SizedBox(height: 20),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border(
+                  top: BorderSide(color: Colors.grey[400]),
+                )
+              ),
+              child: ListView(
+                children: <Widget>[
+                  MemoryComponent(),
+                  MemoryComponent(),
+                  MemoryComponent(),
+                  MemoryComponent(last: true),
+                ],
+              ),
+            ),
           ),
         ],
+        crossAxisAlignment: CrossAxisAlignment.start,
       ),
     );
   }
---
+++ memoryjane/lib/ui/memory.component.dart
@@ -0,0 +1,83 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MemoryComponent extends StatelessWidget {
+
+  final bool last;
+
+  MemoryComponent({this.last = false});
+
+  Widget buildTimelineVisual() {
+    var widgets = <Widget>[
+      Icon(Icons.adjust)
+    ];
+
+    if (!last) {
+      widgets.add(
+          Expanded(
+            child: Container(
+              color: Colors.grey[700],
+              width: 3,
+            ),
+          )
+      );
+    }
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Column(
+        children: widgets
+      ),
+    );
+  }
+
+  Widget displayMemory({var x = false}) {
+    if (Random().nextInt(2) < 1) {
+      return Text(
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        softWrap: true,
+        style: TextStyle(fontSize: 17),
+      );
+    }
+    else {
+      return Image.network("https://api.time.com/wp-content/uploads/2020/01/SnowAngel.jpg?w=600&quality=85");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicHeight(
+      child: Container(
+        child: Row(
+          children: <Widget>[
+            buildTimelineVisual(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0, right: 20.0),
+                child: Column(
+                  children: <Widget>[
+                    displayMemory(),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        "MAY 06, 2017",
+                        softWrap: true,
+                        style: TextStyle(
+                          fontSize: 12,
+                          fontFamily: 'Raleway',
+                          color: Colors.grey[1100]
+                        ),
+                      ),
+                    ),
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ),
+            ),
+          ],
+          crossAxisAlignment: CrossAxisAlignment.start,
+        )
+      ),
+    );
+  }
+}
