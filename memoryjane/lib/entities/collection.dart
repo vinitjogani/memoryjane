4091b864a8f57e2da0e2426d3ecc1c90cab60441
Sriram Balakrishnan <sriram.kasilingam@mail.utoronto.ca>
2020-01-18 16:28:22 -0500

    Added relevant attributes to memory and collection.

--- memoryjane/lib/entities/collection.dart
+++ memoryjane/lib/entities/collection.dart
@@ -1,9 +1,10 @@
 import 'package:memoryjane/entities/memory.dart';
 
 class Collection {
+  final String id;
   final String name;
-  final String coverImage;
+  String coverImageUrl;
   final List<Memory> memories;
 
-  Collection({this.name, this.coverImage, this.memories});
+  Collection({this.id, this.name, this.coverImageUrl, this.memories});
 }
