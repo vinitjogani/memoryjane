12bf7ee002b7e15833d5e25a5bcbdd40a9f68dbf
Vinit Jogani <vnjogani@gmail.com>
2020-01-18 10:49:38 -0500

    Added detail page

--- memoryjane/lib/entities/collection.dart
+++ memoryjane/lib/entities/collection.dart
@@ -1,6 +1,9 @@
+import 'package:memoryjane/entities/memory.dart';
+
 class Collection {
   final String name;
   final String coverImage;
+  final List<Memory> memories;
 
-  Collection({this.name, this.coverImage});
+  Collection({this.name, this.coverImage, this.memories});
 }
