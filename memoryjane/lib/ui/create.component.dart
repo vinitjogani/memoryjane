37411193f48c7aa817c8cf41ab7f35eaf86d3273
Vinit Jogani <vnjogani@gmail.com>
2020-01-18 20:25:49 -0500

    Added icon and updated label

--- memoryjane/lib/ui/create.component.dart
+++ memoryjane/lib/ui/create.component.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:memoryjane/entities/memory.dart';
 import 'package:memoryjane/ui/layout.dart';
